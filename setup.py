57c5a44c890c16b7c0752a49c0bac03b68cfb212
Mike Orr <mike@predicate.us>
2018-04-21 12:44:20 -0400

    version bump

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'predigame',
-    version = '1.2.2',
+    version = '1.2.3',
     description = 'An instructional game development platform',
     url = 'http://predigame.io',
     author = 'Predicate Academy',
     author_email = 'support@predigame.io',
     packages = find_packages(),
-    install_requires = ['pygame', 'pillow', 'astar==0.9', 'PyGithub==1.38'],
+    install_requires = ['pygame', 'pillow', 'astar==0.9', 'PyGithub==1.38','bresenham==0.2'],
     include_package_data = True,
     entry_points = {
         'console_scripts': [
